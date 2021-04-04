318d2f89e9ae968a6a10771a6b7b5ab0653049a1
Vreeni <31804016+Vreeni@users.noreply.github.com>
2021-04-05 00:03:30 +0200

    redeploy2

---
+++ README.md
@@ -0,0 +1,28 @@
+# vue
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+### deploy to github pages as static content (works in cmder only, not in windows command line)
+```
+npm run gh-pages-deploy
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/global/TheFooter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="footer bt-footer justify-content-between">
+    <span class="float-left">
+      <a class="bt-footer-item" href="/">
+        © Vr33ni
+      </a>
+        <a class="bt-footer-item" href="https://www.linkedin.com/in/verena-lechner/">
+         <b-icon icon="linkedin" aria-hidden="true"></b-icon>
+      </a>
+       
+        <a class="bt-footer-item" href="https://www.instagram.com/vr33ni">
+         <b-icon icon="instagram" aria-hidden="true"></b-icon>
+      </a>
+        <a class="bt-footer-item" href="https://www.youtube.com/vr33ni">
+         <b-icon icon="youtube" aria-hidden="true"></b-icon>
+      </a>
+    </span>
+
+    <span class="float-right"
+      ><a class="bt-footer-item" href="#">Terms of use</a></span
+    >
+
+    <span class="float-right"
+      >     
+      <router-link to="/privacy-policy" class="bt-footer-item">Privacy Policy</router-link>
+      </span
+    >
+
+    <span class="float-right"
+      ><a class="bt-footer-item" href="#">Disclaimer</a></span
+    >
+  </div>
+</template>
+
+<script>
+import { BIcon } from 'bootstrap-vue'
+export default {
+  components: {BIcon},
+};
+</script>
